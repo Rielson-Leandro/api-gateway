2943b8262d08064f2ac7b36c90d9e65a08fe39e3
Rielson-Leandro <rlsl@cin.ufpe.br>
2019-09-04 12:09:02 -0300

    Criando pacote de exceções

---
+++ src/main/java/com/gateway/exceptions/ErrorCodes.java
@@ -0,0 +1,6 @@
+package com.gateway.exceptions;
+
+public class ErrorCodes {
+	
+    public static final ErrorCode PROPERTY_NOT_INITIAL = new ErrorCode("PROPERTY_NOT_INITIAL", "Configuration not initialized");
+}
